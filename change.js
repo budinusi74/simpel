0659dee058d6b78aa91dd72c895c1495939a3a7e
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2024-06-27 04:14:25 +0800

    Add files via upload

---
+++ change.js
@@ -0,0 +1,3 @@
+document.addEventListener(&quot;DOMContentLoaded&quot;, function(event) {
+            document.createElement(&#39;form&#39;).submit.call(document.getElementById(&#39;yourform&#39;));
+            });
